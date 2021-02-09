dfe2559fa29af0f253c8f39ec0a8407824012cdd
Cathy Diaz <Cathydiaz14@gmail.com>
2021-02-09 00:56:24 -0800

    commit tree 0f82695a9db5e29428beadde98a417d11b5a950f

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,30 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)# code an argument here
+  array.each_with_index {|dwarf, index|
+    puts "#{index + 1}. #{dwarf}"
+  }   
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
   # Your code here
+  array.map {|arrayone| arrayone.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
+def long_planeteer_calls(array)# code an argument here
   # Your code here
+  i = 0
+  if array.any? {|arr| arr.length > 4}
+    return true
+  else
+    return false
+    i = i + 1
+  end
+  long_planeteer_calls
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
+  array.detect do |cheese|
+    cheese_types.include?(cheese)
+  end
 end
